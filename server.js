7fa779d8652e56e8640df1936991079dd1c0cc90
tyolangaa-ctrl <tyolangaa@gmail.com>
2026-08-19 17:07:28 +0100

    Connect website to Supabase

--- server.js
+++ server.js
@@ -9,11 +9,8 @@ require('dotenv').config();
 
 const app = express();
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 app.use(cors());
